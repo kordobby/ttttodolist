1a99c84b00854c83aca1223491ff0c5971b6723f
이트루 <leetrue@iteuluui-MacBookPro.local>
2023-11-13 02:10:45 +0900

    fix. add useCallback

--- src/page/todo/index.tsx
+++ src/page/todo/index.tsx
@@ -2,7 +2,7 @@ import TodoForm from './component/TodoForm';
 import TodoList from './component/TodoList';
 import * as UI from './style/index';
 import { useGetTodo } from './hook/useGetTodo';
-import { createContext } from 'react';
+import { createContext, useCallback } from 'react';
 import { TodoEntity } from '../../server/TodoProvider';
 
 interface TodoContextType {
@@ -18,9 +18,9 @@ export const TodoContext = createContext<TodoContextType>(initialTodoContext);
 function TodoPage() {
   const [todoList, setTodoList] = useGetTodo();
 
-  const onSetTodoList: (value: TodoEntity[]) => void = (value) => {
+  const onSetTodoList: (value: TodoEntity[]) => void = useCallback((value) => {
     setTodoList(value);
-  };
+  }, [setTodoList]);
 
   return (
     <UI.Layout>
