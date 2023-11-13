877e8900a4683103d26163c242f9e640adb574ce
이트루 <leetrue@iteuluui-MacBookPro.local>
2023-11-13 21:13:44 +0900

    fix. 수정/삭제/완료 로딩 상태 추가

--- src/page/todo/component/TodoFormItem.tsx
+++ src/page/todo/component/TodoFormItem.tsx
@@ -1,17 +1,18 @@
-import { useContext, useState } from 'react';
+import { PropsWithChildren, useContext, useState } from 'react';
 import TodoProvider, { TodoEntity } from '../../../server/TodoProvider';
 import { FormItemType } from '../type';
 import { TodoContext } from '..';
 import Input from '../../../component/core/control/Input';
 import Button from '../../../component/core/control/Button';
 import styled from 'styled-components';
+import Loading from './Loading';
 
 const initialErrorState: FormItemType<boolean> = {
   title: false,
   content: false,
 };
 
-interface TodoListItemProps {
+interface TodoListItemProps extends PropsWithChildren {
   data: TodoEntity;
 }
 
@@ -24,6 +25,7 @@ export function TodoListItem(props: TodoListItemProps) {
     title: data.title,
     content: data.content,
   });
+  const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<FormItemType<boolean>>(initialErrorState);
   const btnDisabled = error.title || error.title;
 
@@ -49,6 +51,7 @@ export function TodoListItem(props: TodoListItemProps) {
 
   const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    setLoading(true);
     const response = await TodoProvider.modifyTodo({
       id: data.id,
       title: formItem.title,
@@ -56,38 +59,65 @@ export function TodoListItem(props: TodoListItemProps) {
       done: data.done,
     });
     setter(response);
+    setLoading(false);
     toggleUpdateMode();
+  };
+
+  const onDelete = async () => {
+    setLoading(true);
+    const response = await TodoProvider.deleteTodo(data.id);
+    setter(response);
+    setLoading(false);
+  };
+
+  const onModify = async () => {
+    setLoading(true);
+    const response = await TodoProvider.modifyDoneState(data.id);
+    setter(response);
+    setLoading(false);
   };
 
   if (isUpdateMode) {
     return (
-      <StForm onSubmit={onSubmit}>
-        <Input>
-          <Input.TextField id={'title'} onChange={onChange} defaultValue={data.title} />
-        </Input>
-        <Input>
-          <Input.TextField id={'content'} onChange={onChange} defaultValue={data.content} />
-        </Input>
-        <Button type="submit" disabled={btnDisabled}>
-          저장
-        </Button>
-        <Button type="submit" onClick={toggleUpdateMode}>
-          취소
-        </Button>
-      </StForm>
+      <Loading loading={loading}>
+        <StForm onSubmit={onSubmit}>
+          <Input>
+            <Input.TextField id={'title'} onChange={onChange} defaultValue={data.title} />
+          </Input>
+          <Input>
+            <Input.TextField id={'content'} onChange={onChange} defaultValue={data.content} />
+          </Input>
+          <Button type="submit" disabled={btnDisabled}>
+            저장
+          </Button>
+          <Button type="submit" onClick={toggleUpdateMode}>
+            취소
+          </Button>
+        </StForm>
+      </Loading>
     );
   }
   return (
-    <div>
-      <span>{data.title} - </span>
-      <span>{data.content}</span>
-      <Button onClick={toggleUpdateMode}>수정</Button>
-    </div>
+    <Loading loading={loading}>
+      <TodoFormItemBox>
+        <span>{data.title} - </span>
+        <span>{data.content}</span>
+        <Button onClick={toggleUpdateMode}>수정</Button>
+        <Button onClick={onDelete}>삭제</Button>
+        <Button onClick={onModify}>완료</Button>
+      </TodoFormItemBox>
+    </Loading>
   );
 }
+
+const TodoFormItemBox = styled.div`
+  button {
+    margin-right: 5px;
+  }
+`;
 
 const StForm = styled.form`
   display: flex;
   gap: 5px;
   align-items: center;
-`
+`;
--- src/page/todo/component/TodoList.tsx
+++ src/page/todo/component/TodoList.tsx
@@ -1,6 +1,4 @@
 import { PropsWithChildren, useContext } from 'react';
-import Button from '../../../component/core/control/Button';
-import TodoProvider from '../../../server/TodoProvider';
 import { TodoContext } from '..';
 import { TodoListItem } from './TodoFormItem';
 import Loading from './Loading';
@@ -11,20 +9,11 @@ interface TodoListProps extends PropsWithChildren {
 }
 
 function TodoList(props: TodoListProps) {
-  const { state, setter, loading } = useContext(TodoContext);
+  const { state, loading } = useContext(TodoContext);
   const listData = state.filter((value) =>
     props.listType === 'active' ? value.done === false : value.done === true
   );
 
-  const onDelete = async (id: string) => {
-    const response = await TodoProvider.deleteTodo(id);
-    setter(response);
-  };
-
-  const onModify = async (id: string) => {
-    const response = await TodoProvider.modifyDoneState(id);
-    setter(response);
-  };
   return (
     <ListWrapper>
       <h3>{props.listType === 'active' ? 'TODO' : 'ARCHIVED'}</h3>
@@ -33,9 +22,7 @@ function TodoList(props: TodoListProps) {
           {listData.map((value) => (
             <li key={value.id} id={value.id}>
               <ListBox>
-                <TodoListItem data={value} />
-                <Button onClick={() => onDelete(value.id)}>삭제</Button>
-                <Button onClick={() => onModify(value.id)}>완료</Button>
+                <TodoListItem data={value}></TodoListItem>
               </ListBox>
             </li>
           ))}
@@ -49,13 +36,13 @@ export default TodoList;
 
 const ListWrapper = styled.div`
   padding: 15px 40px;
-`
+`;
 
 const ListBox = styled.div`
-    display: flex;
-    gap: 5px;
-    span {
-      margin-right: 10px;
-    }
-    margin-bottom: 8px;
-`
+  display: flex;
+  gap: 5px;
+  span {
+    margin-right: 10px;
+  }
+  margin-bottom: 8px;
+`;
